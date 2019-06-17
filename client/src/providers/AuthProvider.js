55e4e624a1bc65c938635554bc5ea01c6bda2695
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-17 14:28:38 -0600

    trying those classes

---
+++ client/src/providers/AuthProvider.js
@@ -0,0 +1,56 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
+const AuthContext = React.createContext()
+export const AuthConsumer = AuthContext.Consumer
+
+const {user, setUser} = useState(null)
+
+export const AuthProvider = () =>{
+const handleLogin = (user, history)=>{
+  axios.post("/api/auth/sign_in", user)
+  .then(res=>{
+    setUser(res.data.data)
+    history.push('/')
+  })
+  .catch(err =>{
+    console.log(err)
+  })
+}
+const handleRegister = (user, history) =>{
+  axios.post("/api/auth",user)
+  .then(res=>{
+    setUser(res.data.data)
+    history.push('/')
+  })
+  .catch(err =>{
+    console.log(err)
+  })
+}
+
+const handleLogout = (history) =>{
+  axios.delete("/api/auth/sign_out")
+  .then(res=>{
+    setUser(null)
+    history.push('/login')
+  })
+  .catch(err =>{
+    console.log(err)
+  })
+}
+
+return(
+  <AuthContext.Provider value = {{
+    user, 
+    authenticated: user !==null,
+    handleLogin: handleLogin(),
+    handleRegister: handleRegister(),
+    handleLogout: handleLogout(),
+    setUser: (user) => setUser({user})
+  }}>
+    {props.children}
+  </AuthContext.Provider>
+)
+
+}
