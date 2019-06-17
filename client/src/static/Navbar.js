f5c301f56aa1dde63178f8245e26a6f370521e1f
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-17 14:38:50 -0600

    navbar stubbed out

---
+++ client/src/static/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import {Menu, } from 'semantic-ui-react'
+import { AuthConsumer} from "../providers/AuthProvider"
+import {Link, withRouter } from 'react-router-dom'
+
+const Navbar = (props) =>{
+  
+
+  const rightNavItems = ({user, handleLogout}) =>{
+    if(user){
+      return(
+        <Menu.Menu position = 'right'>
+          <Menu.Item 
+          name = 'Logout'
+          onClick = {()=> handleLogout(props.history)}
+          />
+        </Menu.Menu>
+      )
+    }else{
+      return(
+        <Menu.Menu position = 'right'>
+          <Link to = '/login'>
+            <Menu.Item
+            name='Login'
+            active={props.location.pathname === '/login'}
+            />
+          </Link>
+          <Link to = '/register'>
+            <Menu.Item
+            name='Register'
+            active={props.location.pathname === '/register'}
+            />
+          </Link>
+        </Menu.Menu>
+      )
+    }
+  }
+  
+  return(
+    <AuthConsumer>
+      {auth => 
+      <Menu pointing secondary>
+        <Link to ='/'>
+          <Menu.Item
+          name="Home"
+          active={props.location.pathname ==='/'}
+          />
+        </Link>
+        {rightNavItems(auth)}
+      </Menu>
+      }
+    </AuthConsumer>
+  )
+  
+}
+
+export default withRouter(Navbar)
