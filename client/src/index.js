795b611617714eed5b7ace0715fafcaf8553129d
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-18 17:13:02 -0600

    middleware

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,11 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './providers/AuthProvider'
 import "semantic-ui-css/semantic.min.css"
+import { initMiddleware, } from 'devise-axios'
+
+
+initMiddleware()
+
  
 ReactDOM.render(
 
