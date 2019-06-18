5725488bb9d783708c9b50980ed91caf7e8b3f6d
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-18 15:42:22 -0600

    seed file stubbed out

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+30.times do
+  name = Faker::Movies::HarryPotter.character
+  nickname = Faker::Games::Pokemon.move
+  image = Faker::Avatar.image(name, "100x400", "png", "set2")
+  email = Faker::Internet.email
+  User.create(name: name, nickname: nickname, image: image, image: image)
+end
+
+puts "we made 30 users"
