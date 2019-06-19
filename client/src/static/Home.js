d7fbf3cccedc0817482e19e8d50fcad9c08bcccd
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-19 15:18:26 -0600

    home stubbed out

--- client/src/static/Home.js
+++ client/src/static/Home.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Header} from 'semantic-ui-react'
+import axios from 'axios';
 
-const Home = () =>(
-  <>
+const Home = () =>{
+
+  const [profiles, setProfiles] = useState([])
+
+  useEffect(()=>{
+    axios.get('/api/profiles')
+    .then(res=>setProfiles(res.data))
+  },[])
+  
+  const addProfile = (id) =>{
+    axios.put(`/api/profiles/${id}`)
+    .then(()=> setProfiles(profiles.filter(p => p.id !== id)))
+  }
+
+  return(
+
+    <div>
     <Header as='h1' textAlign="center">My Space</Header>
-  </>
-)
+    <Card.Group itemsPerRow={4}>
+        {this.state.profiles.map(p=>
+          <Card key = {p.id}>
+            <Image src = {p.avatar}/>
+            <Card.Content>
+              <Divider/>
+              <Card.Header>
+                {p.name}
+              </Card.Header>
+            </Card.Content>
+          </Card>
+          )}
+      </Card.Group>
+  </div>
+    )
+
+}
 
 export default Home
