f6c6dbb8c79ebf8170fd7e193a3bcd21c7732604
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-19 13:48:01 -0600

    added profiles model

---
+++ app/controllers/api/profiles_controller.rb
@@ -0,0 +1,7 @@
+class Api::ProfilesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
---
+++ db/migrate/20190619194311_create_profiles.rb
@@ -0,0 +1,13 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.string :email
+      t.text :about
+      t.string :avatar
+      t.belongs_to :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
