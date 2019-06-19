d6892e0896dd33d2c24f8b0fc094b06c5e853897
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-19 13:51:03 -0600

    user liked profile methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
+  has_many :profiles
+
+  serialize :liked_profiles, Array
+
+  def self.random_profile(ids)
+    ids = ids.empty? ? [0] :ids
+    Profile.where("id NOT IN (?)", ids).order("RANDOM()")
+  end
+
+  def self.liked(ids)
+    ids = ids.empty? ? [0] :ids
+    Profile.where("id IN (?)",ids)
+  end
+  
 end
