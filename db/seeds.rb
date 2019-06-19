b20ee6292082793c506deb54164dbca6417d1ccb
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-19 14:58:28 -0600

    seed file worked with passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,16 @@
   nickname = Faker::Games::Pokemon.move
   image = Faker::Avatar.image(name, "100x400", "png", "set2")
   email = Faker::Internet.email
-  User.create(name: name, nickname: nickname, image: image, image: image)
+  password = "password"
+  u = User.create(name: name, email: email, nickname: nickname, password: password, password_confirmation: password)
+
+  Profile.create(
+    name: name, 
+    about: Faker::Quotes::Shakespeare.hamlet_quote,
+    avatar: image,
+    user_id: u.id
+  )
+
 end
 
-puts "we made 30 users"
+puts "we think we made 30 users Test"
